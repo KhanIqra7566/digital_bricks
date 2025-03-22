42e067fe743d239026c054a01e56f1e1f4c2d6e7
Iqra Khan <iqraKhan34907@gmail.com>
2025-03-22 16:56:00 +0530

    Setup

--- application/config/routes.php
+++ application/config/routes.php
@@ -5,3 +5,13 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $route['default_controller'] = 'Home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+
+$route['login'] = 'Home/login';
+$route['register'] = 'Home/register';
+$route['notification'] = 'Home/notification';
+$route['wallet'] = 'Home/wallet';
+$route['profile_update'] = 'Home/profile_update';
+$route['posted_task'] = 'Home/posted_task';
+$route['view_posted'] = 'Home/view_posted';
+$route['role_employment'] = 'Home/role_employment';
--- application/views/includes/header-link.php
+++ application/views/includes/header-link.php
@@ -2,22 +2,20 @@
 <html lang="en">
 
 
-<!-- Mirrored from bvite-html.pixelwibes.in/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 22 Mar 2025 06:00:54 GMT -->
 <head>
-	<title>: BVite : Admin Dashboard Template</title>
+	<title>My Digital Bricks</title>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="Responsive Bootstrap 5 admin dashboard template & web App ui kit.">
 	<meta name="keyword" content="bootstrap admin template">
 
-	<!--[ Favicon]-->
 	<link rel="icon" type="image/x-icon" href="assets/images/favicon.ico">
 	<link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon-16x16.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon-32x32.png">
 	<link rel="apple-touch-icon" sizes="180x180" href="assets/images/apple-touch-icon.png">
 
-	<!--[ Template main css file ]-->
 	<link rel="stylesheet" href="assets/css/style.min.css">
+    <link rel="stylesheet" href="assets/css/dataTables.min.css">
 
 </head>
